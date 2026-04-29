53520ce8454532a327037cce0670423791f9e425
yuta <775yuta@gmail.com>
2026-04-29 19:09:25 +0900

    feat:修正

--- sw.js
+++ sw.js
@@ -1,39 +1,41 @@
-const CACHE_NAME = 'lap-timer-v3';
+const CACHE_NAME = "lap-timer-v2";
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js?v=1.3.1',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 // インストール時に全リソースをキャッシュ
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
   );
   self.skipWaiting();
 });
 
 // 古いキャッシュを削除
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
-      )
-    )
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)),
+        ),
+      ),
   );
   self.clients.claim();
 });
 
 // キャッシュファースト戦略
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       return cached || fetch(event.request);
-    })
+    }),
   );
 });
